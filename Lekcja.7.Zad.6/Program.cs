193ed2359ca0317c26df287376296910248e786a
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-13 07:07:09 +0200

    Add project files.

---
+++ Lekcja.7.Zad.6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Podaj swój wzrost (w centymetrach):");
+		int wzrost = Convert.ToInt32(Console.ReadLine());
+
+		if (wzrost < 150)
+		{
+			Console.WriteLine("Jesteś krasnoludem");
+		}
+		else if (wzrost >= 150 && wzrost < 165)
+		{
+			Console.WriteLine("Jesteś średniego wzrostu");
+		}
+		else if (wzrost >= 165 && wzrost < 180)
+		{
+			Console.WriteLine("Jesteś wysokiego wzrostu");
+		}
+		else
+		{
+			Console.WriteLine("Jesteś bardzo wysokiego wzrostu");
+		}
+	}
+}
